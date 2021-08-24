e787d787c043f07b03f39972b1fbb80c2673a2b5
AXG25 <axiever.guejia@gmail.com>
2021-08-24 10:53:28 -0500

    edicion

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
-const main = document.getElementById("main");
+
+let carta = document.getElementById("carta");
 
 const viewProduct = async () => {
-  main.innerHTML = "";
-  const resp = await fetch(" http://localhost:4000/ropa");
-  const data = resp.json();
-  data.forEach((productos) => {
-    const { imagen, talla, valor, nombre } = productos;
-    main.innerHTML = `
-       <div class="card" style="width: 18rem">
-        <img src="${imagen}" class="card-img-top" alt="..." />
-        <div class="card-body">
-          <h5 class="card-title">${nombre}</h5>
-          <p class="card-text">
-           Precio: ${valor}
-           Talla: ${talla}
-          </p>
-          <a href="#" class="btn btn-primary">Comprar</a>
-        </div>
-      </div>
+  carta.innerHTML = '';
+  const resp = await fetch("http://localhost:4000/ropa");
+  const data = await resp.json();
+  data.map((productos) => {
+    const { imagen, Talla, valor, nombre } = productos;
+    carta.innerHTML = `
+    <div class="card bg-dark text-white" style="width: 18rem;">
+    <img src="${imagen}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${nombre}</h5>
+      <p class="card-text">
+      Precio:${valor}
+      Talla:${Talla}
+      </p>
+      <a href="#" class="btn btn-light">Borrar</a>
+      <a href="#" class="btn btn-light">Editar</a>
+    </div>
        `;
   });
 };
